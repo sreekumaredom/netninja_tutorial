adc50e04af8e992f231da512ef69e783a3c59164
Anandhan <anandhanedom@gmail.com>
2020-08-20 22:51:31 +0530

    page 3 js not yet

---
+++ 015_blooms/page3.js
@@ -0,0 +1,156 @@
+//Location sort buttons
+const wyndBtn = document.getElementById('wynd');
+const bnglrBtn = document.getElementById('bnglr');
+
+//Model sort buttons
+const cvBtn = document.getElementById('cv');
+const homeBtn = document.getElementById('home');
+const landBtn = document.getElementById('land');
+const ltlBtn = document.getElementById('ltl');
+const rentalBtn = document.getElementById('rental');
+const stlBtn = document.getElementById('stl');
+const ivBtn = document.getElementById('iv');
+
+//Status sort buttons
+const cmpBtn = document.getElementById('cmp');
+const acqBtn = document.getElementById('acq');
+
+//Cards container
+var cardsContainer = document.getElementById('cards-container');
+
+//Array of location buttons
+const locButtons = [wyndBtn, bnglrBtn];
+
+//Array of status buttons
+const statusButtons = [cmpBtn, acqBtn];
+
+//Array of model buttons
+const modelButtons = [
+  cvBtn,
+  homeBtn,
+  landBtn,
+  ltlBtn,
+  rentalBtn,
+  stlBtn,
+  ivBtn,
+];
+
+//Filter object
+const filter = {};
+
+//Assigning event listener to location buttons
+locButtons.forEach((loc) => {
+  loc.addEventListener('click', () => {
+    if (loc.checked == true) {
+      console.log('sorting on');
+    } else {
+      console.log('sorting off');
+    }
+  });
+});
+
+function deleteByVal(val) {
+  for (var key in filter) {
+    if (filter[key] == val) delete filter[key];
+  }
+}
+
+//Assigning event listener to status buttons
+statusButtons.forEach((status) => {
+  status.addEventListener('click', (e) => {
+    if (status.checked == true) {
+      if (e.target.id == 'cmp') {
+        filter.status = 'Completed';
+      } else {
+        filter.status = 'Customer Aquisition';
+      }
+      console.log(filter);
+    } else {
+      if (e.target.id == 'cmp') {
+        deleteByVal('Completed');
+      } else {
+        deleteByVal('Customer Aquisition');
+      }
+      console.log(filter);
+    }
+  });
+});
+
+//Assigning event listener to model buttons
+modelButtons.forEach((model) => {
+  model.addEventListener('click', () => {
+    if (model.checked == true) {
+      console.log('sorting on');
+    } else {
+      console.log('sorting off');
+    }
+  });
+});
+
+//For storing fetched data
+let projectData;
+
+async function fetchProjects() {
+  let res = await fetch('./page3.json');
+
+  //Fetch and store here
+  projectData = await res.json();
+
+  let filteredData = projectData.filter((item) => {
+    for (let key in filter) {
+      if (item[key] === undefined || item[key] != filter[key]) return false;
+    }
+    return true;
+  });
+
+  populateUI(filteredData);
+}
+
+//Display cards
+function populateUI(cards) {
+  let output = '';
+
+  cardsContainer.innerHTML = ''; //initial reset
+
+  cards.map((card) => {
+    output += `<div class="card">
+    <img src="${card.src}" class="echo-img" alt="house" />
+    <div class="card-details">
+      <h1>${card.name}</h1>
+      <p class="card-location">${card.location}</p>
+      <p class="card-desc">${card.desc}</p>
+      <div class="legal-process">
+        <label for="file" class="legal-lbl">Legal Process</label>
+        <progress id="file" value="${card.legal}" class="progress" max="100">
+          32%
+        </progress>
+        <span class="progress-value">${card.legal}%</span>
+      </div>
+      <div class="ratings-bar">
+        <p>Plot Availability</p>
+        <div class="bars checked"></div>
+        <div class="bars checked"></div>
+        <div class="bars unchecked"></div>
+        <div class="bars unchecked"></div>
+        <div class="bars unchecked"></div>
+      </div>
+      <p class="cmn-mrgn">
+        Current Project Status : ${card.status}
+      </p>
+      <div class="card-btns">
+        <button class="project-btn">
+          <a href=""
+            >Project Details
+            <span
+              ><i class="fa fa-arrow-down" aria-hidden="true"></i></span
+          ></a>
+        </button>
+        <button class="enquire-btn"><a href="">Enquire</a></button>
+      </div>
+    </div>
+  </div>`;
+  });
+  cardsContainer.innerHTML = output;
+}
+
+fetchProjects();
